59cd5f833972b1a663878c4e3da8bf20ded800f0
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2016-08-02 13:45:42 -0400

    add the CKC loo test script

--- psi/tests/loo_ckc.py
+++ psi/tests/loo_ckc.py
@@ -1,44 +1,59 @@
-import sys, time
+import sys
 import numpy as np
 import matplotlib.pyplot as pl
-from psi.library_models import MILESInterpolator
-from badstar import allbadstars
+from psi.library_models import CKCInterpolator
 
-ts = time.time()
-
-def reselect(spi, mlib, bad_ids, bounds, normwave=1.0):
-    """Select a training set using bounds and removing bad stars listed by miles_id
-    """
-    spi.load_training_data(training_data=mlib)
-    spi.renormalize_library_spectra(bylabel='luminosity')
-    ind = [spi.training_labels['miles_id'].tolist().index(b) for b in bad_ids
-           if b in spi.training_labels['miles_id']]
-    spi.leave_out(ind)
-    spi.restrict_sample(bounds=bounds)
-    return spi
+lightspeed = 2.998e18
 
 # The PSI Model
-mlib = '/Users/bjohnson/Projects/psi/data/irtf/irtf_prugniel_extended.h5'
-fgk_bounds = {'teff': (4200.0, 9000.0)}
-spi = MILESInterpolator(training_data=mlib)
-badstar_ids = np.array(allbadstars.tolist())
+mlib = '/Users/bjohnson/Codes/SPS/ckc/ckc/lores/ckc_R10k.h5'
+fgk_bounds = {'logt': (np.log10(4200.0), np.log10(9000.0))}
+
+spi = CKCInterpolator(training_data=mlib, logify_flux=True)
 spi.features = (['logt'], ['feh'], ['logg'],
+                # Square terms
                 ['logt', 'logt'], ['feh', 'feh'], ['logg', 'logg'],
+                # Cubic terms
+                ['feh', 'feh', 'feh'], ['logt', 'logt', 'logt'], ['logg', 'logg', 'logg'],
+                # Cross terms
                 ['logt', 'feh'], ['logg', 'logt'], ['logg', 'feh'],
-                ['logt', 'logt', 'logt'], ['logt', 'logt', 'logt', 'logt'],
-                ['logt', 'logt', 'logg']
+                # logt high order
+                ['logt', 'logt', 'logt', 'logt'], ['logt', 'logt', 'logt', 'logt', 'logt'],
+                # feh high order
+                ['feh', 'feh', 'feh', 'feh'],
+                # logg-logt high order cross terms
+                ['logt', 'logt', 'logg'], ['logt', 'logg', 'logg'],
+                # feh-logt high order cross terms
+                ['logt', 'logt', 'feh'], ['logt', 'feh', 'feh'],
+                # feh-logg high order cross terms
+                ['logg', 'logg', 'feh'], ['logg', 'feh', 'feh'],
                 )
-spi = reselect(spi, mlib, badstar_ids, fgk_bounds)
 
+spi.select(bounds=fgk_bounds, delete=True)
+tot = np.trapz(spi.training_spectra * lightspeed/spi.wavelengths**2, spi.wavelengths, axis=-1)
+#bad = tot <= 0
+#spi.leave_out(np.where(bad)[0])
 ntrain = spi.n_train
-predicted = np.zeros([ntrain, spi.n_wave])
 
-#sys.exit()
+spi.train()
+params = spi.training_labels
+logt_solar = np.unique(params['logt'])[10]
+solar = (params['logt'] == logt_solar) & (params['feh'] == 0) & (params['logg'] == 4.5)
+specsun = spi.get_star_spectrum(logt=logt_solar, feh=0, logg=4.5)
+
+import matplotlib.pyplot as pl
+pl.figure()
+pl.plot(spi.wavelengths, spi.training_spectra[solar,:][0])
+pl.plot(spi.wavelengths, specsun)
+pl.show()
+
+
+predicted = np.zeros([ntrain, spi.n_wave])
 # Retrain and predict after leaving one out
-for i, j in enumerate(spi.training_indices.copy()):
-    if (i % 10) == 0: print(i)
+loo_indices = spi.training_indices.copy()[::10]
+for i, j in enumerate(loo_indices):
+    if (i % 10) == 0: print('{} of {}'.format(i, len(loo_indices)))
     # get full sample and the parameters of the star to leave out
-    # spi = reselect(spi, mlib, badstar_ids, fgk_bounds)
     spec = spi.training_spectra[i,:]
     tlabels = spi.training_labels[i]
     labels = dict([(n, tlabels[n]) for n in spi.label_names])
@@ -49,42 +64,32 @@ for i, j in enumerate(spi.training_indices.copy()):
     spi.library_mask[j] = True
 
 # reload the full training set
-#spi = reselect(spi, mlib, badstar_ids, fgk_bounds)
+# spi.select(training_data=mlib, bounds=fgk_bounds)
 
 # get fractional residuals
-delta = predicted / spi.training_spectra - 1.0
-# get variance in good regions of the spectrum
-wave = spi.wavelengths
-wmin, wmax = 0.38, 2.4
-telluric = [(1.35, 1.43), (1.81, 1.91)]
-varinds = (wave > wmin) & (wave < wmax)
-for bl, bh in telluric:
-    varinds = varinds & ((wave  > bh) | (wave  < bl))
+wmin, wmax = 3800, 7200
 imin = np.argmin(np.abs(spi.wavelengths - wmin))
 imax = np.argmin(np.abs(spi.wavelengths - wmax))
+imin, imax = 0, len(spi.wavelengths) -1
+delta = predicted / spi.training_spectra - 1.0
 
-var_spectrum = np.nanvar(delta, axis=0)
-var_total = np.nanvar(delta[:, varinds], axis=1)
+sys.exit()
 
-# Determine fraction of pixels contributing half the RMS
-sv = np.sort(delta[:, varinds]**2, axis=1)
-svc = np.cumsum(sv, axis=1)
-svcn = svc/ np.nanmax(svc, axis=-1)[:,None]
-half = np.nanargmin(np.abs(svcn - 0.5), axis=1)
-npix = np.isfinite(svcn).sum(axis=1)
-
-lines, indlines = {'Ha':6563., 'NaD': 5897.0, 'CaK': 3933.0, 'CaH': 3968, 'Mg5163':5163.1, 'CaT':8544.0}, {}
+var_spectrum = delta.var(axis=0)
+var_total = delta[:, imin:imax].var(axis=1)
+lines, indlines = {'Ha':6563., 'NaD': 5897.0, 'CaK': 3933.0, 'CaH': 3968, 'Mg5163':5163.1}, {}
 for l, w in lines.items():
-    indlines[l] = np.argmin(np.abs(spi.wavelengths - w/1e4))
+    indlines[l] = np.argmin(np.abs(psi.wavelengths - w))
+
 
 # Plot the variance spectrum
 sfig, sax = pl.subplots()
-sax.plot(spi.wavelengths[imin:imax], np.sqrt(var_spectrum[imin:imax])*100, label='$\sigma(m/o-1)$')
-sax.set_xlabel('$\lambda (\mu m)$')
+sax.plot(spi.wavelengths, np.sqrt(var_spectrum)*100, label='$\sigma(m/o-1)$')
+sax.set_xlabel('$\lambda (\AA)$')
 sax.set_ylabel('Fractional RMS (%)')
-sax.set_ylim(0, 80)
+sax.set_ylim(0, 100)
 sfig.show()
-sfig.savefig('figures/irtf_residual_spectrum.pdf')
+sfig.savefig('figures/residual_spectrum.pdf')
 
 # Plot a map of total variance as a function of label
 l1, l2, l3 = 'logt', 'feh', 'logg'
@@ -99,15 +104,17 @@ mapaxes[1].set_ylabel(l3)
 mapaxes[1].invert_yaxis()
 cbar = pl.colorbar(sc)
 cbar.ax.set_ylabel('Fractional RMS (%)')
+[ax.invert_xaxis() for ax in mapaxes]
 mapfig.show()                   
-mapfig.savefig('figures/irtf_residual_map.pdf')
+mapfig.savefig('figures/residual_map.pdf')
 
 # Plot a map of line residual as a function of label
-showlines = ['CaT']#lines.keys()
+showlines = lines.keys()
+showlines = ['CaK', 'NaD']
 for line in showlines:
-    vlim = -100, 100
-#    if lines[line] < 0.4:
-    vlim = -100, 100
+    vlim = -50, 50
+    if lines[line] < 4000:
+        vlim = -100, 100
     mapfig, mapaxes = pl.subplots(1, 2, figsize=(12.5,7))
     sc = mapaxes[0].scatter(lab[l1], lab[l2], marker='o', c=delta[:, indlines[line]]*100,
                             cmap=pl.cm.coolwarm, vmin=vlim[0], vmax=vlim[1])
@@ -120,8 +127,9 @@ for line in showlines:
     mapaxes[1].invert_yaxis()
     cbar = pl.colorbar(sc)
     cbar.ax.set_ylabel('Residual @ {}'.format(line))
+    [ax.invert_xaxis() for ax in mapaxes]
     mapfig.show()
-    mapfig.savefig('figures/irtf_residual_{}_map.pdf'.format(line))
+    mapfig.savefig('figures/residual_{}_map.pdf'.format(line))
 
 # Plot cumulative number as a function of RMS
 rms = np.sqrt(var_total)*100
@@ -133,26 +141,26 @@ cax.set_ylabel('N(<RMS)')
 cax.set_xlabel('Fractional RMS (%)')
 cax.set_xlim(0,100)
 cfig.show()
-cfig.savefig('figures/irtf_cumlative_rms.pdf')
-
+cfig.savefig('figures/cumlative_rms.pdf')
 
 badfig, badaxes = pl.subplots(10, 1, sharex=True, figsize=(6, 12))
 for i, bad in enumerate(oo[-10:][::-1]):
     ax = badaxes[i]
     labels = dict([(n, spi.training_labels[bad][n]) for n in spi.training_labels.dtype.names])
     title = "T={teff:4.0f}, logg={logg:3.2f}, feh={feh:3.1f}".format(**labels)
-    ax.plot(spi.wavelengths[imin:imax], predicted[bad, imin:imax], label='predicted')
-    ax.plot(spi.wavelengths[imin:imax], spi.training_spectra[bad, imin:imax], label='actual')
+    ax.plot(spi.wavelengths, predicted[bad,:], label='predicted')
+    ax.plot(spi.wavelengths, spi.training_spectra[bad,:], label='actual')
     ax.text(0.05, 0.9, "#{}, RMS={:4.1f}%".format(spi.training_labels[bad]['miles_id'], rms[bad]),
             transform=ax.transAxes, fontsize=10)
     ax.text(0.7, 0.05, title, transform=ax.transAxes, fontsize=10)
-    print(i)
-    
     if i == 0:
         ax.legend(loc=0, prop={'size':8})
     if i < 9:
         ax.set_xticklabels([])
-badfig.savefig('figures/irtf_worst10.pdf')
+badfig.savefig('figures/worst10.pdf')
+
+
+
 sys.exit()
 # compute covariance matrix
 dd = delta[:, imin:imax]
@@ -160,5 +168,5 @@ dd[~np.isfinite(dd)] = 0.0
 cvmat = np.cov(dd.T)
 
 l = 'Ha'
-plot(spi.wavelengths[imin:imax], cvmat[lineinds[l]-imin,:], label=lines.keys()[i])
+plot(psi.wavelengths[imin:imax], cvmat[lineinds[l]-imin,:], label=lines.keys()[i])
      
