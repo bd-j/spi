d66bcf45dfb7ae6aa454db0babfe23e8ccd0ce5b
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2016-08-28 18:08:28 -0400

    fix S/N ceiling, working loo_combined test matrices

--- demo/loo_combined.py
+++ demo/loo_combined.py
@@ -29,8 +29,8 @@ class CombinedInterpolator(SimplePSIModel):
 
         # enforce a max S/N
         if snr_max < np.inf:
-            self.library_snr = np.hypot(1.0 / self.library_snr,
-                                        1.0 / snr_max) # np.clip(self.library_snr, 0, snr_max)
+            self.library_snr = 1 / np.hypot(1.0 / self.library_snr,
+                                            1.0 / snr_max) # np.clip(self.library_snr, 0, snr_max)
 
         # --- set negative (or very low) S/N fluxes to zero weight ---
         bad = ((self.library_snr < snr_threshold) |
@@ -74,24 +74,28 @@ class CombinedInterpolator(SimplePSIModel):
 
 def run_matrix():
     from itertools import product
+    nmiles = [78, 15, 68, 6, 35]
     regimes = ['Warm Giants', 'Cool Giants', 'Warm Dwarfs', 'Cool Dwarfs', 'Hot Stars']
     fake_weights = [ True, False ]
-    c3k_weight = [1e-3, 1e-1, 1]
+    c3k_weight = [1e-3, 1e-1, 10.0]
 
-    for regime, wght, use_unc in product(regimes, c3k_weight, fake_weights):
-        loo(regime=regime, c3k_weight=wght, fake_weights=use_unc)
+    for regime, wght, fake_unc in product(regimes, c3k_weight, fake_weights):
+        outroot = '{}_unc={}_cwght={:03.2f}'.format(regime.replace(' ','_'),
+                                                not fake_unc, wght)
+        _ = loo(regime=regime, c3k_weight=wght, fake_weights=fake_unc, outroot=outroot)
 
 
 def loo(regime='Warm Giants',c3k_weight=1e-1, # the relative weight of the CKC models compared to the MILES models.
-        fake_weights=False, snr_max=1e3):
+        fake_weights=False, snr_max=1e3, outroot='test'):
 
+#if __name__ == "__main__":
+#    c3k_weight = 1e-1 # the relative weight of the CKC models compared to the MILES models.
+#    regime = 'Warm Giants'
+#    fake_weights = False
+#    snr_max = 1e3
     ts = time.time()
-    #c3k_weight = 1e-1 # the relative weight of the CKC models compared to the MILES models.
-    #regime = 'Warm Giants'
-    #fake_weights = False
-    #snr_max = 1e3
-    outroot = '{}_unc={}_cwght={:03.2f}'.format(regime.replace(' ','_'),
-                                                not fake_weights, c3k_weight)
+#    outroot = '{}_unc={}_cwght={:03.2f}'.format(regime.replace(' ','_'),
+#                                                not fake_weights, c3k_weight)
 
     # --- The PSI Model ---
     mlib = '/Users/bjohnson/Projects/psi/data/combined/culled_lib_w_mdwarfs_w_unc_w_c3k.h5'
@@ -136,6 +140,7 @@ def loo(regime='Warm Giants',c3k_weight=1e-1, # the relative weight of the CKC m
         inhull[i] = spi.inside_hull(labels)
         # now put it back
         spi.library_mask[j] = True
+
         
     print('time to retrain {} models: {:.1f}s'.format(len(loo_indices), time.time()-ts))
         
@@ -169,8 +174,8 @@ def loo(regime='Warm Giants',c3k_weight=1e-1, # the relative weight of the CKC m
              color=colors[0])
     sax.plot(spi.wavelengths, chi_mean_spectrum, label=r'$\bar{\chi}$',
              color=colors[1])
-    sax.axvline(1, linestyle=':', color=colors[0])
-    sax.axvline(0, linestyle=':', color=colors[1])
+    sax.axhline(1, linestyle=':', color=colors[0])
+    sax.axhline(0, linestyle=':', color=colors[1])
     sax.set_ylim(-5, 30)
     sax.set_ylabel('$\chi$ (predicted - observed)')
     #sax.plot(spi.wavelengths, np.sqrt(var_spectrum)*100, label='Dispersion')
@@ -204,13 +209,15 @@ def loo(regime='Warm Giants',c3k_weight=1e-1, # the relative weight of the CKC m
     # --- Write output ---
     import json
     with h5py.File('{}_results.h5'.format(outroot), 'w') as f:
+        w = f.create_dataset('wavelengths', data=spi.wavelengths)
         obs = f.create_dataset('observed', data=observed)
         mod = f.create_dataset('predicted', data=predicted)
         unc = f.create_dataset('uncertainty', data=obs_unc)
         p = f.create_dataset('parameters', data=spi.library_labels[loo_indices])
         f.attrs['terms'] = json.dumps(spi.features)
-        
+
+    return spi
 
 if __name__ == "__main__":
-    loo(regime='Warm Giants', c3k_weight=1e-1, fake_weights=False)
-    #run_matrix()
+    #loo(regime='Warm Giants', c3k_weight=1e-1, fake_weights=False)
+    run_matrix()
